f73552c975c2016ed40c38d9960c5d8f134f32b9
NoobMaester <joshstilez@gmail.com>
2024-03-19 20:47:43 +0200

    connect to db with mongoose

---
+++ src/connectToDb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const url = process.env.MONGOOSE_URL;
+
+const connectToDb = () => {
+    mongoose.connect(url)
+
+    mongoose.connection.on('Connected', () => {
+        console.log("connected to database")
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log(err)
+    });
+};
+
+module.exports = connectToDb;
